29a82d6c7a71b0bfe76edf806d997bd7f8a91ebe
superpck <superpck@gmail.com>
2023-02-03 14:10:01 +0700

    เปลี่ยน charSet เป็น charset

--- app/plugins/db.js
+++ app/plugins/db.js
@@ -3,20 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const knex_1 = require("knex");
 var timezone = 'Asia/Bangkok';
 var options = {
-    ISONLINE: {
-        client: process.env.IS_DB_CLIENT || process.env.HIS_DB_CLIENT || 'mysql',
-        connection: {
-            host: process.env.IS_DB_HOST || process.env.HIS_DB_HOST,
-            user: process.env.IS_DB_USER || process.env.HIS_DB_USER,
-            password: process.env.IS_DB_PASSWORD || process.env.HIS_DB_PASSWORD,
-            database: process.env.IS_DB_NAME || 'isdb',
-            port: +process.env.IS_DB_PORT || +process.env.HIS_DB_PORT || 3306,
-            charSet: process.env.IS_DB_CHARSET || process.env.HIS_DB_CHARSET || 'utf8',
-            schema: process.env.IS_DB_SCHEMA || process.env.HIS_DB_SCHEMA,
-            encrypt: process.env.IS_DB_ENCRYPT || process.env.HIS_DB_ENCRYPT || true,
-            timezone
-        }
-    },
     HIS: {
         client: process.env.HIS_DB_CLIENT || 'mysql',
         connection: {
@@ -25,9 +11,23 @@ var options = {
             password: process.env.HIS_DB_PASSWORD,
             database: process.env.HIS_DB_NAME,
             port: +process.env.HIS_DB_PORT || 3306,
-            charSet: process.env.HIS_DB_CHARSET || 'utf8',
+            charset: process.env.HIS_DB_CHARSET || 'utf8',
             schema: process.env.HIS_DB_SCHEMA || 'public',
             encrypt: process.env.HIS_DB_ENCRYPT || true,
+            timezone
+        }
+    },
+    ISONLINE: {
+        client: process.env.IS_DB_CLIENT || process.env.HIS_DB_CLIENT || 'mysql',
+        connection: {
+            host: process.env.IS_DB_HOST || process.env.HIS_DB_HOST,
+            user: process.env.IS_DB_USER || process.env.HIS_DB_USER,
+            password: process.env.IS_DB_PASSWORD || process.env.HIS_DB_PASSWORD,
+            database: process.env.IS_DB_NAME || 'isdb',
+            port: +process.env.IS_DB_PORT || +process.env.HIS_DB_PORT || 3306,
+            charset: process.env.IS_DB_CHARSET || process.env.HIS_DB_CHARSET || 'utf8',
+            schema: process.env.IS_DB_SCHEMA || process.env.HIS_DB_SCHEMA,
+            encrypt: process.env.IS_DB_ENCRYPT || process.env.HIS_DB_ENCRYPT || true,
             timezone
         }
     },
@@ -39,26 +39,12 @@ var options = {
             password: process.env.REFER_DB_PASSWORD || process.env.HIS_DB_PASSWORD,
             database: process.env.REFER_DB_NAME || process.env.HIS_DB_NAME,
             port: +process.env.REFER_DB_PORT || +process.env.HIS_DB_PORT || 3306,
-            charSet: process.env.REFER_DB_CHARSET || process.env.HIS_DB_CHARSET || 'utf8',
+            charset: process.env.REFER_DB_CHARSET || process.env.HIS_DB_CHARSET || 'utf8',
             schema: process.env.REFER_DB_SCHEMA || process.env.HIS_DB_SCHEMA || 'public',
             encrypt: process.env.REFER_DB_ENCRYPT || process.env.HIS_DB_ENCRYPT || true,
             timezone
         }
-    },
-    CANNABIS: {
-        client: process.env.CANNABIS_DB_CLIENT || process.env.HIS_DB_CLIENT || 'mysql',
-        connection: {
-            host: process.env.CANNABIS_DB_HOST || process.env.HIS_DB_HOST,
-            user: process.env.CANNABIS_DB_USER || process.env.HIS_DB_USER,
-            password: process.env.CANNABIS_DB_PASSWORD || process.env.HIS_DB_PASSWORD,
-            database: process.env.CANNABIS_DB_NAME || process.env.HIS_DB_NAME,
-            port: +process.env.CANNABIS_DB_PORT || +process.env.HIS_DB_PORT || 3306,
-            charSet: process.env.CANNABIS_DB_CHARSET || process.env.HIS_DB_CHARSET || 'utf8',
-            schema: process.env.CANNABIS_DB_SCHEMA || process.env.HIS_DB_SCHEMA || 'public',
-            encrypt: process.env.CANNABIS_DB_ENCRYPT || process.env.HIS_DB_ENCRYPT || true,
-            timezone
-        }
-    },
+    }
 };
 const dbConnection = (type = 'HIS') => {
     let option = options[type.toUpperCase()];
