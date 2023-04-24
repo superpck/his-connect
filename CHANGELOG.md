eeeca6cdc9d62e1e236e3676b3fd432fd9e3c402
superpck <superpck@gmail.com>
2023-04-24 14:48:19 +0700

    เปลี่ยนการ connect ไปยังกระทรวง

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,14 @@
-# 3.3.4 - 2023.02.09-01
+# 3.3.5
+## Feature
+  - เปลี่ยนคำสั่งการส่งส่วนกลางจาก http ไปใช้ axios เพื่อให้ง่ายต่อการเชื่อมโยง
+  - เปลี่ยน url ที่รับข้อมูลจาก http://203.157.103.33:8080/nrefer เป็น https://nrefer.moph.go.th/apis/nrefer เพื่อเพิ่มความปลอดภัย
+  - ยกเลิกตัวแปรที่ไม่จำเป็นใน file config
+
+# 3.3.4
 ## Feature
   - เพิ่มการ Connect Oracle, MSSql
 
-# 3.3.3 - 2023.02.06-01
+# 3.3.3
 ## Feature
   - แก้ไข charSet เป็น charset
   - set charset เฉพาะ MySQL
--- src/routes/main/crontab.ts
+++ src/routes/main/crontab.ts
@@ -30,49 +30,6 @@ async function getServiceUrl(config) {
       "notify": "http://203.157.103.33:8080/nrefer/message"
     }
   };
-
-  // const url = process.env.MOPH_URL1 || 'http://203.157.103.176/moph-api';
-  // const mophUrl = url.split('/');
-
-  // const dataSending = querystring.stringify({
-  //   hospcode: process.env.HOSPCODE, ip: ip.address()
-  // });
-
-  // const options = {
-  //   hostname: mophUrl[2],
-  //   path: '/' + mophUrl[3] + '/service',
-  //   method: 'GET',
-  //   headers: {
-  //     'Content-Type': 'application/x-www-form-urlencoded',
-  //     'Content-Length': Buffer.byteLength(dataSending)
-  //   }
-  // };
-
-  // let ret = '';
-  // return new Promise((resolve, reject) => {
-  //   const req = http.request(options, (res) => {
-  //     res.setEncoding('utf8');
-  //     res.on('data', (chunk: string) => {
-  //       ret += chunk;
-  //     });
-  //     res.on('end', () => {
-  //       if (ret) {
-  //         const data = JSON.parse(ret);
-  //         resolve(data);
-  //       } else {
-  //         resolve(null);
-  //       }
-  //     });
-  //   });
-
-  //   req.on('error', (e: any) => {
-  //     reject(e);
-  //   });
-
-  //   req.write(dataSending);
-  //   req.end();
-  // });
-
 }
 
 const router = async (mophService: any, config = {}) => {
