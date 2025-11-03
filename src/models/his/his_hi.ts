5b02e132f8e495d73419d1dc82f80677ee43bfe0
superpck <superpck@gmail.com>
2025-11-03 14:24:50 +0700

    ปรับ Source HIS HI

--- src/models/his/his_hi.ts
+++ src/models/his/his_hi.ts
@@ -1,6 +1,5 @@
 import { Knex } from 'knex';
 const maxLimit = 250;
-const hcode = process.env.HOSPCODE;
 let hisHospcode = process.env.HOSPCODE;
 
 export class HisHiModel {
@@ -19,7 +18,19 @@ export class HisHiModel {
 
   // รหัส Ward
   getWard(db: Knex, wardCode: string = '', wardName: string = '') {
-    return [];
+    let sql = db('idpm');
+    if (wardCode) {
+      sql.where('idpm', wardCode);
+    } else if (wardName) {
+      sql.whereLike('idpmname', `%${wardName}%`)
+    }
+    return sql
+      .select('idpm as wardcode', 'idpmname as wardname',
+        `export_code as std_code`, 'bed_std as bed_normal',
+        'is_active as isactive'
+      )
+      .orderBy('idpm')
+      .limit(maxLimit);
   }
 
   // รายละเอียดแพทย์
@@ -159,11 +170,32 @@ export class HisHiModel {
 
   // MOPH ERP
   countBedNo(db: Knex) {
-    return { total_bed: 0 };
+    return db('ipt')
+      .where('dchdate', '0000-00-00')
+      .count('an as total_bed')
+      .first();
+    // return { total_bed: 0 };
   }
 
   async getBedNo(db: Knex, bedno: any = null, start = -1, limit: number = 1000) {
-    return [];
+    return db('ipt')
+      .leftJoin('idpm', 'ipt.ward', 'idpm.idpm')
+      .leftJoin('iptadm', 'ipt.an', 'iptadm.an')
+      .leftJoin('bedtype', 'iptadm.bedtype', 'bedtype.bedtype')
+      .where('dchdate', '0000-00-00')
+      .select(
+        db.raw(`ltrim(substring(iptadm.bedno, 2, 20)) as bedno`)
+        , db.raw(`ifnull(iptadm.bedtype, '-') as bedtype`)
+        , db.raw(`ifnull(bedtype.namebedtyp,'-') as bedtype_name`)
+        , '1 as roomno'
+        , 'ipt.ward as wardcode'
+        , 'idpm.nameidpm as wardname'
+        , 'idpm.is_active as isactive'
+        , db.raw(`ifnull(bedtype.type_code, 'N') as bed_type`)
+        , db.raw(`if(length(idpm.export_code) = 6, idpm.export_code, concat(idpm.export_code,bedtype.export_code)) as std_code`)
+        , '1 as bed_status_type_id'
+        , `'active' as bed_status_type_name`
+      );
   }
 
   // Report zone
@@ -172,13 +204,83 @@ export class HisHiModel {
   }
 
   concurrentIPDByWard(db: Knex, date: any) {
-    return [];
+    return db('ipt')
+      .innerJoin('idpm', 'ipt.ward', 'idpm.idpm')
+      .where('ipt.rgtdate', '<=', date)
+      .andWhere(function () {
+        this.where('ipt.dchdate', '>=', date)
+          .orWhere('ipt.dchdate', '0000-00-00');
+      })
+      .select(
+        'ipt.ward as wardcode',
+        'idpm.nameidpm as wardname',
+        db.raw(`count(case when rgtdate = ? then an end) as new_case`, [date]),
+        db.raw(`count(case when dchdate = ? then an end) as discharge`, [date]),
+        db.raw(`count(case when dchstts in (8,9) then an end) as death`),
+        db.raw(`
+      count(
+        case 
+          when rgtdate <= ? 
+          and (dchdate > ? or dchdate = '0000-00-00') 
+          then an 
+        end
+      ) as cases
+    `, [date, date])
+      )
+      .groupBy('ipt.ward');
   }
   concurrentIPDByClinic_(db: Knex, date: any) {
-    return [];
+    return db('ipt')
+      .leftJoin('spclty', 'ipt.dept', 'spclty.spclty')
+      .where('ipt.rgtdate', '<=', date)
+      .andWhere(function () {
+        this.where('ipt.dchdate', '>=', date)
+          .orWhere('ipt.dchdate', '0000-00-00');
+      })
+      .select(
+        'ipt.dept as cliniccode',
+        'spclty.name as clinicname',
+        db.raw(`count(case when rgtdate = ? then an end) as new_case`, [date]),
+        db.raw(`count(case when dchdate = ? then an end) as discharge`, [date]),
+        db.raw(`count(case when dchstts in (8,9) then an end) as death`),
+        db.raw(`
+      count(
+        case 
+          when rgtdate <= ? 
+          and (dchdate > ? or dchdate = '0000-00-00') 
+          then an 
+        end
+      ) as cases
+    `, [date, date])
+      )
+      .groupBy('ipt.dept');
   }
+
   concurrentIPDByClinic(db: Knex, date: any) {
-    return [];
+        return db('ipt')
+      .leftJoin('spclty', 'ipt.dept', 'spclty.spclty')
+      .where('ipt.rgtdate', '<=', date)
+      .andWhere(function () {
+        this.where('ipt.dchdate', '>=', date)
+          .orWhere('ipt.dchdate', '0000-00-00');
+      })
+      .select(
+        'ipt.dept as cliniccode',
+        'spclty.namespclty as clinicname',
+        db.raw(`count(case when rgtdate = ? then an end) as new_case`, [date]),
+        db.raw(`count(case when dchdate = ? then an end) as discharge`, [date]),
+        db.raw(`count(case when dchstts in (8,9) then an end) as death`),
+        db.raw(`
+      count(
+        case 
+          when rgtdate <= ? 
+          and (dchdate > ? or dchdate = '0000-00-00') 
+          then an 
+        end
+      ) as cases
+    `, [date, date])
+      )
+      .groupBy('ipt.dept');
   }
   sumOpdVisitByClinic(db: Knex, date: any) {
     return [];
