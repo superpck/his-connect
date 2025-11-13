ba237d3e191a0be36a9b242728cf8a8bb7d0f0d5
jwongkamalasai <jwongkamalasai@gmail.com>
2025-11-13 10:58:53 +0700

    Refactor SQL query to include clinic code and name
    แก้ไข column ที่ส่งออก cliniccode, clinicname

--- src/models/his/his_hi.ts
+++ src/models/his/his_hi.ts
@@ -277,8 +277,8 @@ export class HisHiModel {
       .andWhere(db.raw(`ipt.ward <> ''`))
       .select(
         db.raw(`${hcode} as hospcode`),
-        'ipt.ward as wardcode',
-        'idpm.nameidpm as wardname',
+        db.raw(`if(ipt.dept = '' or ipt.dept is null,'00',ipt.dept) as cliniccode`),
+        db.raw(`ifnull(spec.namespclty,'ไม่ระบุ') as clinicname`),
         db.raw(`count(case when concat(rgtdate,' ',time(rgttime*100)) between ?  and ? then ipt.an end) as new_case`, [dateStart, dateEnd]),
         db.raw(`count(case when concat(dchdate,' ',time(dchtime*100)) between ?  and ? then ipt.an end) as discharge`, [dateStart, dateEnd]),
         db.raw(`count(case when dchstts in (8,9) and concat(dchdate,' ',time(dchtime*100)) between ?  and ? then ipt.an end) as death`, [dateStart, dateEnd]),
