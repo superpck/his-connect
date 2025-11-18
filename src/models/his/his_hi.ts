8b270a54d4635d944a342a4450034020a1463c04
jakkrapong wongkamalasai <jwongkamalasai@gmail.com>
2025-11-18 23:08:57 +0700

    add visit for moph alert

--- src/models/his/his_hi.ts
+++ src/models/his/his_hi.ts
@@ -319,4 +319,33 @@ export class HisHiModel {
       .groupBy('cln.specialty')
       .orderBy('cln.specialty');
   }
+    getVisitForMophAlert(db: Knex, date: any) {
+      date = moment(date).locale('TH').format('YYYY-MM-DD');
+      let sql = db('ovst as visit') // ข้อมูลผู้ป่วยนอก
+      .innerJoin('pt as patient', 'visit.hn', 'patient.hn') // ข้อมูลประชาชน
+      .leftJoin('cln as clinic', 'visit.cln', 'clinic.cln') // ห้องตรวจ
+      .leftJoin('ipt as admission', 'visit.an', 'admission.an') // ผู้ป่วยใน
+      .leftJoin('idpm as ward', 'admission.ward', 'ward.idpm') // ward ผู้ป่วยใน
+      .where(db.raw(`((date(visit.vstdttm) = ? and visit.an = 0 and visit.ovstost = '1') or admission.dchdate = ?) `, [date, date])) 
+      // เฉพาะผู้ป่วยนอกที่มาในวันนั้น และ status 1 = discharge กลับบ้าน หรือ ผู้ป่วยในที่จำหน่ายในวันนั้น 
+      .andWhere(db.raw(`patient.pop_id <> ''`)) // ต้องมีหมายเลขบัตรประชาชน
+      .andWhere(db.raw(`patient.pop_id is not null`)) // ต้องมีหมายเลขบัตรประชาชน
+      .andWhere(db.raw(`length(patient.pop_id) = 13`)) // ต้องมีความยาว 13 หลัก
+      .andWhere(db.raw(`length(patient.pop_id) not in (?,?)`, ['1111111111119', '9999999999994'])) // ไม่เอาหมายเลขประชาชนตัวอย่าง
+      .andWhere(db.raw(`timestampdiff(year, patient.brthdate, ?) between 15 and 90`, [date])) // อายุระหว่าง 15-90 ปี
+      .andWhere(db.raw(`patient.ntnlty = '99'`)) // สัญชาติไทย
+      ;
+    return sql
+      .select('visit.hn', 'visit.vn', 'patient.pop_id as cid',
+        db.raw(`CASE
+              when visit.an > 0 and substr(ward.export_code,4,3) = '606' THEN 'HOMEWARD',
+              WHEN visit.an > 0 and substr(ward.export_code,4,3) <> '606' THEN 'IPD' ,  
+              WHEN visit.an = 0 and visit.cln = '20100' THEN 'ER' 
+              ELSE 'OPD' END as department_type`),
+        'clinic.cln as department_code', 'clinic.namecln as department_name',
+        db.raw('date(visit.vstdttm) as date_service'), 
+        db.raw('time(visit.vstdttm) as time_service') 
+      )
+      .groupBy('visit.cln', 'visit.hn'); // กันซ้ำ hn ในวันเดียวกันในห้องตรวจเดียวกัน
+  }
 }
